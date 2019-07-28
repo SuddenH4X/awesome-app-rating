c8ca75d9fff50e4baa6a5fb794fac354943c2584
SuddenH4X <NagaraHyuga@gmail.com>
2019-07-28 12:36:38 +0200

    test: add tests for class RatingThreshold

--- library/src/main/java/com/suddenh4x/ratingdialog/preferences/RatingThreshold.kt
+++ library/src/main/java/com/suddenh4x/ratingdialog/preferences/RatingThreshold.kt
@@ -1,5 +1,7 @@
 package com.suddenh4x.ratingdialog.preferences
 
 enum class RatingThreshold {
-    NONE, HALF, ONE, ONE_AND_A_HALF, TWO, TWO_AND_A_HALF, THREE, THREE_AND_A_HALF, FOUR, FOUR_AND_A_HALF, FIVE
+    NONE, HALF, ONE, ONE_AND_A_HALF, TWO, TWO_AND_A_HALF, THREE, THREE_AND_A_HALF, FOUR, FOUR_AND_A_HALF, FIVE;
 }
+
+fun RatingThreshold.toFloat() = this.ordinal / 2f
