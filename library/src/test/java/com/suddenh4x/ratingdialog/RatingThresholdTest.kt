88ea9dedd19881ea67e34c70c70b4a4d78b4bd92
SuddenH4X <SuddenH4X@users.noreply.github.com>
2019-07-28 14:33:27 +0200

    test: add testing dependencies to lib

--- library/src/test/java/com/suddenh4x/ratingdialog/RatingThresholdTest.kt
+++ library/src/test/java/com/suddenh4x/ratingdialog/RatingThresholdTest.kt
@@ -2,18 +2,13 @@ package com.suddenh4x.ratingdialog
 
 import com.suddenh4x.ratingdialog.preferences.RatingThreshold
 import com.suddenh4x.ratingdialog.preferences.toFloat
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class RatingThresholdTest {
 
     @Test
-    fun ratingThresholdCalculationIsCorrect() {
+    fun `calculation of toFloat() is correct`() {
         val ratingThresholdNone = RatingThreshold.NONE.toFloat()
         val ratingThresholdThreeAndAHalf = RatingThreshold.THREE_AND_A_HALF.toFloat()
         val ratingThresholdFive = RatingThreshold.FIVE.toFloat()
