13040a4ff8501cb77c6de50e78884a764f422034
SuddenH4X <NagaraHyuga@gmail.com>
2019-07-31 20:40:45 +0200

    fix: use dialog fragment for all dialogs to fix orientation change issues

---
+++ library/src/main/java/com/suddenh4x/ratingdialog/dialog/DialogType.kt
@@ -0,0 +1,7 @@
+package com.suddenh4x.ratingdialog.dialog
+
+import java.io.Serializable
+
+enum class DialogType : Serializable {
+    RATING_OVERVIEW, RATING_STORE, FEEDBACK_MAIL, FEEDBACK_CUSTOM
+}
