1d4227226a9bb46a966aecdba802e6b556e832f9
SuddenH4X <SuddenH4X@users.noreply.github.com>
2026-06-13 13:54:14 +0200

    chore: use Compose bom

--- exampleapp/build.gradle.kts
+++ exampleapp/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
     implementation(project(":library"))
 
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.core)
